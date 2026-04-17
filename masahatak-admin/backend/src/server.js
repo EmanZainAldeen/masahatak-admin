3e2f578f64395b6610c93006ac5aca155ecfd55b
Eman ZainAldeen <30460456+EmanZainAldeen@users.noreply.github.com>
2026-04-17 15:35:07 +0300

    Update server.js

--- masahatak-admin/backend/src/server.js
+++ masahatak-admin/backend/src/server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: (origin, callback) => {
+    app.use(cors()); // اسمحي بجميع الاتصالات لفترة الفحص فقط
     const allowed = [
       process.env.FRONTEND_URL,
       'http://localhost:3000',
